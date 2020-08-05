866406e0171179ba2e294278b20661f33f0c9d6d
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-08-05 22:42:54 +0530

    Added Day-04 Solution

--- Day-04.cpp
+++ Day-04.cpp
@@ -1,1 +1,14 @@
-
+class Solution {
+public:
+    float lg(int a, int b)
+    {
+        return log(a)/log(b);
+    }
+   
+    bool isPowerOfFour(int num) {
+        if(num==0)
+            return false;
+        
+        return floor(lg(num,4))== ceil(lg(num, 4));
+    }
+};
