e907d3d6475a3c6e66ba835cbfb0b838a09e4688
Yash Rathod <39941246+yashrt@users.noreply.github.com>
2020-08-25 17:51:00 +0530

    Added Day-24 solution

--- Day-24.cpp
+++ Day-24.cpp
@@ -1,1 +1,35 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* root, int& sum)
+    {
+        if(root)
+        {
+            if(root->left)
+            {
+                if(!root->left->left && !root->left->right)
+                    sum = sum + root->left->val;
+                dfs(root->left, sum);
+            }
+            if(root->right)
+                dfs(root->right, sum);
+        }
+        
+    }
+    
+    int sumOfLeftLeaves(TreeNode* root) {
+        int sum = 0;
+        dfs(root, sum);
+        return sum;
+    }
+};
